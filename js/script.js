b8d3f5c60c772d76367ac40347fb0a2c87c70293
memataexe <dexe.me12@gmail.com>
2022-08-08 18:30:16 -0300

    Adicionado botão para exibir pokémon Shiny

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonType = document.querySelector('.pokemon__type');
+
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
+const typeContainer = document.querySelector('#types');
+
+
+let Shiny = 'front_default';
 
 let searchPokemon = 1;
 
+
+function button_shiny() {
+  if (Shiny=='front_shiny'){
+    Shiny='front_default'
+
+
+  } else{Shiny='front_shiny'}
+  //document.getElementById('button_shiny')
+  renderPokemon(searchPokemon);
+  }
+
+
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+
+
 
   if (APIResponse.status === 200) {
     const data = await APIResponse.json();
@@ -25,20 +46,36 @@ const renderPokemon = async (pokemon) => {
 
   const data = await fetchPokemon(pokemon);
 
+
+
   if (data) {
     pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];  
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated'][Shiny];
+    divInnerHTML = '';
+    data['types'].forEach((item, index) => {
+      tipo = item['type']['name'];
+      let imageUrl;
+      switch (tipo) {
+        case 'electric':
+          imageUrl = '';
+        case 'electric':
+          imageUrl = '';
+        break;
+      }
+      divInnerHTML += `<img class="type_image" src="${imageUrl}"/>`;
+    });
+    typeContainer.innerHTML = divInnerHTML;
 
-    input.value = '';
+
     searchPokemon = data.id;
   } else {
     pokemonImage.style.display = 'none';
     pokemonName.innerHTML = 'Not found';
     pokemonNumber.innerHTML = '';
   }
+  input.value = '';
 }
 
 form.addEventListener('submit', (event) => {
@@ -50,6 +87,8 @@ buttonPrev.addEventListener('click', () => {
   if (searchPokemon > 1) {
     searchPokemon -= 1;
     renderPokemon(searchPokemon);
+    
+    
   }
 });
 
@@ -57,7 +96,7 @@ buttonPrev.addEventListener('click', () => {
 buttonNext.addEventListener('click', () => {
   searchPokemon += 1;
   renderPokemon(searchPokemon);
+
 });
 
 renderPokemon(searchPokemon);
-
